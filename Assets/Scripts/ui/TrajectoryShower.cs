03c8a9ad601db00403ab199b63c55a7ae4a74a61
Aizak1 <aizak30042002@gmail.com>
2021-07-12 14:11:31 +0300

    Trajectory Shower Script

---
+++ Assets/Scripts/ui/TrajectoryShower.cs
@@ -0,0 +1,59 @@
+using bow;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ui {
+    public class TrajectoryShower : MonoBehaviour {
+
+        [SerializeField]
+        private GameObject pointPrefab;
+        [SerializeField]
+        private int numberOfPoitns;
+        [SerializeField]
+        private float spaceBetweenPoints;
+
+        [SerializeField]
+        private BowController bowController;
+
+        private GameObject[] points;
+
+        private void Update() {
+            if (Input.touchCount <= 0) {
+                return;
+            }
+
+            if (Input.touches[0].phase == TouchPhase.Began) {
+                points = new GameObject[numberOfPoitns];
+                for (int i = 0; i < numberOfPoitns; i++) {
+                    var pointPos = bowController.instantiatedArrow.transform.position;
+                    points[i] = Instantiate(pointPrefab, pointPos , Quaternion.identity);
+                }
+                points[0].SetActive(false);
+            }
+
+            if (Input.touches[0].phase == TouchPhase.Moved) {
+                for (int i = 0; i < numberOfPoitns; i++) {
+                    points[i].transform.position = CalculatePointPosition(i * spaceBetweenPoints);
+                }
+            }
+
+            if (Input.touches[0].phase == TouchPhase.Ended) {
+                for (int i = 0; i < numberOfPoitns; i++) {
+                    Destroy(points[i]);
+                }
+            }
+        }
+
+        private Vector3 CalculatePointPosition(float t) {
+            var arrowTransform = bowController.instantiatedArrow.transform;
+            var direction = arrowTransform.forward;
+            var vO = direction * bowController.pullAmount * bowController.instantiatedArrow.speed;
+            return arrowTransform.position + (vO * t) + 0.5f * Physics.gravity * (t * t);
+        }
+    }
+}
+
+
--- Assets/Scripts/ui/UiController.cs
+++ Assets/Scripts/ui/UiController.cs
@@ -1,5 +1,3 @@
-using bow;
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,18 +8,6 @@ namespace ui {
         [SerializeField]
         private Image touchMarkUp;
 
-        [SerializeField]
-        private GameObject pointPrefab;
-        [SerializeField]
-        private int numberOfPoitns;
-        [SerializeField]
-        private float spaceBetweenPoints;
-
-        [SerializeField]
-        private BowController bowController;
-
-        private GameObject[] points;
-
         private void Update() {
             if (Input.touchCount <= 0) {
                 return;
@@ -30,37 +16,12 @@ namespace ui {
             if (Input.touches[0].phase == TouchPhase.Began) {
                 touchMarkUp.rectTransform.position = Input.touches[0].position;
                 touchMarkUp.gameObject.SetActive(true);
-
-                points = new GameObject[numberOfPoitns];
-                for (int i = 0; i < numberOfPoitns; i++) {
-                    var pointPos = bowController.instantiatedArrow.transform.position;
-                    points[i] = Instantiate(pointPrefab, pointPos , Quaternion.identity);
-                }
-                points[0].SetActive(false);
-            }
-
-            if (Input.touches[0].phase == TouchPhase.Moved) {
-                for (int i = 0; i < numberOfPoitns; i++) {
-                    points[i].transform.position = CalculatePointPosition(i * spaceBetweenPoints);
-                }
             }
 
             if (Input.touches[0].phase == TouchPhase.Ended) {
                 touchMarkUp.gameObject.SetActive(false);
-
-                for (int i = 0; i < numberOfPoitns; i++) {
-                    Destroy(points[i]);
-                }
             }
-        }
-
-        private Vector3 CalculatePointPosition(float t) {
-            var arrowTransform = bowController.instantiatedArrow.transform;
-            var direction = arrowTransform.forward;
-            var vO = direction * bowController.pullAmount * bowController.instantiatedArrow.speed;
-            return arrowTransform.position + (vO * t) + 0.5f * Physics.gravity * (t * t);
         }
     }
 }
-
 
