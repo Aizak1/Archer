ed8c55d53798ef22014206233605fa986c0d52bb
Aizak1 <aizak30042002@gmail.com>
2021-07-29 11:13:28 +0300

    Change Arrow Ui and level 2 fix

--- Assets/Scripts/ui/UiController.cs
+++ Assets/Scripts/ui/UiController.cs
@@ -3,6 +3,8 @@ using arrow;
 using UnityEngine;
 using UnityEngine.UI;
 using level;
+using TMPro;
+using System.Collections.Generic;
 
 namespace ui {
     public class UiController : MonoBehaviour {
@@ -17,7 +19,37 @@ namespace ui {
         private Text enemyCountText;
 
         [SerializeField]
+        private TextMeshProUGUI arrowTypeText;
+        [SerializeField]
+        private RawImage arrowImage;
+        [SerializeField]
+        private Texture[] arrowImageTextures;
+
+        public Dictionary<ArrowType, Texture> arrowTypeToTexture
+          = new Dictionary<ArrowType, Texture>();
+
+
+        [SerializeField]
         private SceneLoader sceneLoader;
+
+        private void Start() {
+            if(arrowTypeText == null || arrowImage == null) {
+                return;
+            }
+
+            if(arrowImageTextures.Length < resource.countToArrowType.Count) {
+                Debug.LogError("Lack of Images");
+                return;
+            }
+
+            for (int i = 0; i < resource.countToArrowType.Count; i++) {
+                arrowTypeToTexture.Add(resource.countToArrowType[i], arrowImageTextures[i]);
+            }
+
+            arrowTypeText.text = bowController.arrowTypeToInstantiate.ToString();
+            var currentType = bowController.arrowTypeToInstantiate;
+            arrowImage.texture = arrowTypeToTexture[currentType];
+        }
 
         private void Update() {
             if (enemyCountText != null && levelController != null) {
@@ -40,6 +72,8 @@ namespace ui {
 
             ArrowType nextType = resource.countToArrowType[nextTypeIndex];
             bowController.arrowTypeToInstantiate = nextType;
+            arrowTypeText.text = nextType.ToString();
+            arrowImage.texture = arrowTypeToTexture[nextType];
         }
     }
 }
