5bae5f27a1982ec9c9508b4f8d1606e93b521aec
AlexanderKravchenkoS <kravchenko.alexander.s@gmail.com>
2021-08-03 12:54:32 +0300

    Animation change

--- Assets/Scripts/bow/BowAnimator.cs
+++ Assets/Scripts/bow/BowAnimator.cs
@@ -6,11 +6,13 @@ namespace bow {
         private Animator animator;
         [SerializeField]
         private BowController bowController;
+        [SerializeField]
+        private float maxPull;
 
         private const string BLEND = "Blend";
 
         private void Update() {
-            animator.SetFloat(BLEND, bowController.pullAmount);
+            animator.SetFloat(BLEND, bowController.pullAmount * maxPull);
         }
     }
 }
