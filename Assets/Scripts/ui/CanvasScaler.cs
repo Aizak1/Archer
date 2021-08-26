da24606a89a8df7a29f6f009a6d1416c88e53368
AlexanderKravchenkoS <kravchenko.alexander.s@gmail.com>
2021-08-26 14:55:06 +0300

    Adaptive canvas size

---
+++ Assets/Scripts/ui/CanvasScaler.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace ui {
+    public class CanvasScaler : MonoBehaviour{
+        [SerializeField]
+        private RectTransform[] canvasTransforms;
+
+        [SerializeField]
+        private RectTransform refCanvasTransform;
+
+        private void Start() {
+            float width = refCanvasTransform.rect.width;
+            float height = refCanvasTransform.rect.height;
+
+            Vector2 newSize = new Vector2(width, height);
+
+            for (int i = 0; i < canvasTransforms.Length; i++) {
+                canvasTransforms[i].sizeDelta = newSize;
+            }
+        }
+    }
+}
