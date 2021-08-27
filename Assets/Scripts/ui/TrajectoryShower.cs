9047d25163377b61f5f5356240d37766194825f9
AlexanderKravchenkoS <kravchenko.alexander.s@gmail.com>
2021-08-27 12:05:50 +0300

    New trajectory up to 7 level

--- Assets/Scripts/ui/TrajectoryShower.cs
+++ Assets/Scripts/ui/TrajectoryShower.cs
@@ -18,6 +18,12 @@ namespace ui {
 
         private const string MAIN_TEXTURE = "_MainTex";
 
+        [SerializeField]
+        private GameObject[] balls;
+        [SerializeField]
+        private float ballDistance = 0.4f;
+        private int ballindex;
+
         private void Start() {
             lineRenderer = GetComponent<LineRenderer>();
 
@@ -58,6 +64,70 @@ namespace ui {
                 var pos = pointList[i];
                 lineRenderer.SetPosition(i, pos);
             }
+
+            if (balls.Length == 0) {
+                return;
+            }
+
+            float[] magnitudes = new float[pointList.Length - 1];
+            float sum = 0;
+
+            for (int i = 0; i < magnitudes.Length; i++) {
+                magnitudes[i] = (pointList[i] - pointList[i + 1]).magnitude;
+                sum += magnitudes[i];
+            }
+
+            float requiredMag = ballDistance;
+            float tmpMag = 0;
+            ballindex = 0;
+
+            for (int i = 0; i < magnitudes.Length; i++) {
+                if (tmpMag >= sum) {
+                    break;
+                }
+
+                if (tmpMag + magnitudes[i] >= requiredMag) {
+                    float need = requiredMag - tmpMag;
+                    float full = magnitudes[i];
+
+                    float percent = need / full;
+
+                    Vector3 line = pointList[i + 1] - pointList[i];
+
+                    balls[ballindex].transform.position = pointList[i] + line * percent;
+                    balls[ballindex].SetActive(true);
+                    ballindex++;
+
+                    tmpMag += magnitudes[i];
+                    requiredMag += ballDistance;
+                } else {
+                    tmpMag += magnitudes[i];
+                }
+            }
+
+            for (int i = ballindex; i < balls.Length; i++) {
+                balls[i].SetActive(false);
+            }
+
+            if (ballindex == 2) {
+                balls[0].transform.localScale = Vector3.one * trajectorySettup.StartWidth;
+                balls[1].transform.localScale = Vector3.one * trajectorySettup.EndWidth;
+                return;
+            } else if (ballindex == 1) {
+                balls[0].transform.localScale = Vector3.one * trajectorySettup.StartWidth;
+                return;
+
+            } else if (ballindex - 2 < 0) {
+                return;
+            }
+
+            float scaleStep = (trajectorySettup.StartWidth - trajectorySettup.EndWidth) / (ballindex - 2);
+            float scale = trajectorySettup.StartWidth + scaleStep;
+
+            for (int i = 0; i < ballindex; i++) {
+                balls[i].transform.localScale = Vector3.one * scale;
+                scale -= scaleStep;
+            }
         }
 
         public void EndDraw() {
@@ -66,6 +136,17 @@ namespace ui {
             }
 
             lineRenderer.positionCount = 0;
+
+            if (balls.Length == 0) {
+                return;
+            }
+
+            for (int i = 0; i <= ballindex; i++) {
+                if (i >= balls.Length) {
+                    return;
+                }
+                balls[i].SetActive(false);
+            }
         }
 
         public void SetSettings(TrajectorySettings settings) {
