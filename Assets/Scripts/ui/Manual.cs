8fc415c7fe06ce4c24c9348e94644bc3506771ee
Aizak1 <aizak30042002@gmail.com>
2021-09-10 17:12:14 +0300

    Quiz Button

--- Assets/Scripts/ui/Manual.cs
+++ Assets/Scripts/ui/Manual.cs
@@ -13,46 +13,77 @@ namespace ui {
 
         [SerializeField]
         private GameObject[] manualMenues;
+        [SerializeField]
+        private int minManualIndex;
         private int currentIndex;
+
+        [SerializeField]
+        private bool isQuiz;
 
         [SerializeField]
         private HintController hintController;
 
         private void Start() {
+            if (isQuiz) {
+                return;
+            }
+
             int currentScene = SceneManager.GetActiveScene().buildIndex;
             if (PlayerPrefs.GetInt(LevelsManager.LEVEL_AT) > currentScene) {
                 return;
             }
 
+            ShowManual();
+        }
+
+        public void CloseManual() {
+            manualMenues[currentIndex].SetActive(false);
+            bowController.enabled = true;
+
+            if (hintController) {
+                hintController.gameMenu = gameMenu;
+                hintController.enabled = true;
+            } else {
+                gameMenu.SetActive(true);
+            }
+
+        }
+
+        public void ShowNextManual() {
+            if (currentIndex >= manualMenues.Length - 1) {
+                return;
+            }
+            CloseManual();
+            currentIndex++;
+            ShowManual();
+        }
+
+        public void ShowPreviousManual() {
+            if (currentIndex <= minManualIndex) {
+                return;
+            }
+            CloseManual();
+            currentIndex--;
+            ShowManual();
+        }
+
+        public void ShowManual() {
             if (manualMenues.Length == 0) {
                 return;
             }
 
-            currentIndex = 0;
+            if(currentIndex >= manualMenues.Length) {
+                return;
+            }
+
+            if (currentIndex < 0) {
+                return;
+            }
 
             manualMenues[currentIndex].SetActive(true);
             gameMenu.SetActive(false);
 
             bowController.enabled = false;
-        }
-
-        public void CloseManual() {
-            manualMenues[currentIndex].SetActive(false);
-
-            currentIndex++;
-
-            if (currentIndex >= manualMenues.Length) {
-                bowController.enabled = true;
-
-                if (hintController) {
-                    hintController.gameMenu = gameMenu;
-                    hintController.enabled = true;
-                } else {
-                    gameMenu.SetActive(true);
-                }
-            } else {
-                manualMenues[currentIndex].SetActive(true);
-            }
         }
     }
 }
